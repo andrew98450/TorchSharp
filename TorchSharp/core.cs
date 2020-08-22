cd1d700d07793ed0ef5c636fe9eb70b78ba4d166
Andrew Huang <ktpss96034@gmail.com>
2020-08-22 15:20:33 +0800

    Add files via upload

---
+++ TorchSharp/core.cs
@@ -0,0 +1,388 @@
+﻿using NumSharp;
+using System;
+using System.Collections.Generic;
+namespace TorchSharp
+{
+    public class Tensor 
+    {
+        public NDArray data;
+        public NDArray grad;
+        public Dictionary<Tensor, NDArray> create;
+        public static Stack<Dictionary<Tensor, NDArray>> grads;
+        public Tensor()
+        {
+            data = np.zeros();
+            create = new Dictionary<Tensor, NDArray>();
+            grad = np.zeros();
+            grads = new Stack<Dictionary<Tensor, NDArray>>();
+        }
+        public Tensor(NDArray data)
+        {
+            this.data = data;
+            create = new Dictionary<Tensor, NDArray>();
+            grad = np.zeros(this.data.Shape);
+            grads = new Stack<Dictionary<Tensor, NDArray>>();
+        }
+        public static Tensor operator +(Tensor a, Tensor b)
+        {
+            return new AddBackward(a, b, grads);
+        }
+        public static Tensor operator +(Tensor a, double b)
+        {
+            return new AddBackward(a, b, grads);
+        }
+        public static Tensor operator -(Tensor a, Tensor b)
+        {
+            return new SubBackward(a, b, grads);
+        }
+        public static Tensor operator -(Tensor a, double b)
+        {
+            return new SubBackward(a, b, grads);
+        }
+        public static Tensor operator -(Tensor a)
+        {
+            return a * -1;
+        }
+        public static Tensor operator *(Tensor a, Tensor b)
+        {
+            return new MulBackward(a, b, grads);
+        }
+        public static Tensor operator *(Tensor a, double b)
+        {
+            return new MulBackward(a, b, grads);
+        }
+        public static Tensor operator /(Tensor a, Tensor b)
+        {
+            return new DivBackward(a, new InvBackward(b, grads), grads);
+        }
+        public static Tensor operator /(Tensor a, double b)
+        {
+            return new DivBackward(a, b, grads);
+        }
+        public static Tensor operator ^(Tensor a, double b)
+        {
+            return new PowBackward(a, b, grads);
+        }
+        public override string ToString()
+        {
+            return data.ToString();
+        }
+        public static Tensor sin(Tensor a)
+        {
+            return new SinBackward(a, grads);
+        }
+        public static Tensor cos(Tensor a)
+        {
+            return new CosBackward(a, grads);
+        }
+        public static Tensor tan(Tensor a)
+        {
+            return new TanBackward(a, grads);
+        }
+        public static Tensor tanh(Tensor a)
+        {
+            return new TanhBackward(a, grads);
+        }
+        public static Tensor sigmoid(Tensor a)
+        {
+            return new SigmoidBackward(a, grads);
+        }
+        public static Tensor relu(Tensor a)
+        {
+            return new ReLUBackward(a, grads);
+        }
+        public static Tensor exp(Tensor a)
+        {
+            return new ExpBackward(a, grads);
+        }
+        public static Tensor log(Tensor a)
+        {
+            return new LogBackward(a, grads);
+        }
+        public static Tensor mean(Tensor a)
+        {
+            return new MeanBackward(a, grads);
+        }
+        public static Tensor sum(Tensor a)
+        {
+            return new SumBackward(a, grads);
+        }
+        public static Tensor mm(Tensor a, Tensor b)
+        {
+            return new MMBackward(a, b, grads);
+        }
+        public static Tensor mse_loss(Tensor output, Tensor label) 
+        {
+            return mean((output - label) ^ 2);
+        }
+        public static Tensor linear(Tensor input, Tensor weight, Tensor bias)
+        {
+            return mm(input, new Tensor(weight.data)) + bias;
+        }
+        public void backward()
+        {
+            create = new Dictionary<Tensor, NDArray>();
+            int count = 0, item = grads.Count;
+            while (count <= item)
+            {
+                Dictionary<Tensor, NDArray> stack = grads.Pop();
+                foreach (var node in stack.Keys)
+                {
+                    if (!create.ContainsKey(node))
+                        create.Add(node, stack[node]);
+                    else
+                        create[node] += stack[node];
+
+                    if (node.GetType() != typeof(Tensor))
+                    {
+                        Dictionary<Tensor, NDArray> child = new Dictionary<Tensor, NDArray>(node.create);
+                        foreach (var child_node in node.create.Keys)
+                        {
+                            if (stack[node].shape.Length == 2 && node.create[child_node].shape.Length == 2)
+                                if (stack[node].shape[1] == node.create[child_node].shape[0])
+                                    child[child_node] = np.dot(stack[node], node.create[child_node]);
+                                else
+                                    child[child_node] = stack[node] * node.create[child_node];
+                            else
+                                child[child_node] = stack[node] * node.create[child_node];
+                            grads.Push(child);
+                        }
+                    }
+                }
+                stack.Clear();
+                count++;
+            }
+            foreach (var node in create.Keys)
+            {
+                node.grad = create[node];
+            }
+            grads.Clear();
+        }
+
+        class AddBackward : Tensor
+        {
+            public AddBackward(Tensor a, Tensor b, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = a.data + b.data;
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, np.ones(a.data.Shape));
+                create.Add(b, np.ones(b.data.Shape));
+                Tensor.grads.Push(create);
+            }
+            public AddBackward(Tensor a, double b, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = a.data + b;
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, np.ones(a.data.Shape));
+                Tensor.grads.Push(create);
+            }
+        }
+        class MulBackward : Tensor
+        {
+            public MulBackward(Tensor a, Tensor b, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = a.data * b.data;
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, b.data);
+                create.Add(b, a.data);
+                Tensor.grads.Push(create);
+            }
+            public MulBackward(Tensor a, double b, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = a.data * b;
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, np.zeros(a.data.Shape) + b);
+                Tensor.grads.Push(create);
+            }
+        }
+        class MMBackward : Tensor
+        {
+            public MMBackward(Tensor a, Tensor b, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = np.matmul(a.data, b.data.transpose());
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, np.matmul(b.data, np.ones(a.data.transpose().Shape)));
+                create.Add(b, np.matmul(a.data, np.ones(b.data.transpose().Shape)));
+                Tensor.grads.Push(create);
+            }
+        }
+        class PowBackward : Tensor
+        {
+            public PowBackward(Tensor a, double b, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = np.power(a.data, b);
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, np.power(a.data, b - 1) * b);
+                Tensor.grads.Push(create);
+            }
+        }
+
+        class DivBackward : Tensor
+        {
+            public DivBackward(Tensor a, Tensor b, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = a.data / b.data;
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, b.data);
+                create.Add(b, a.data);
+                Tensor.grads.Push(create);
+            }
+            public DivBackward(Tensor a, double b, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = a.data / b;
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, a.data);
+                Tensor.grads.Push(create);
+            }
+        }
+        class SubBackward : Tensor
+        {
+            public SubBackward(Tensor a, Tensor b, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = a.data - b.data;
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, np.ones(a.data.Shape));
+                create.Add(b, np.ones(b.data.Shape) * -1);
+                Tensor.grads.Push(create);
+            }
+            public SubBackward(Tensor a, double b, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = a.data - b;
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, np.ones(a.data.Shape));
+                Tensor.grads.Push(create);
+            }
+        }
+        class SinBackward : Tensor
+        {
+            public SinBackward(Tensor a, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = np.sin(a.data);
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, np.cos(a.data));
+                Tensor.grads.Push(create);
+            }
+        }
+        class CosBackward : Tensor
+        {
+            public CosBackward(Tensor a, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = np.cos(a.data);
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, -np.sin(a.data));
+                Tensor.grads.Push(create);
+            }
+        }
+        class TanBackward : Tensor
+        {
+            public TanBackward(Tensor a, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = np.tan(a.data);
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, np.power(1 / np.cos(a.data), 2));
+                Tensor.grads.Push(create);
+            }
+        }
+        class SigmoidBackward : Tensor
+        {
+            public SigmoidBackward(Tensor a, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = 1 / (1 + np.exp(-a.data));
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, data * (1 - data));
+                Tensor.grads.Push(create);
+            }
+        }
+        class TanhBackward : Tensor
+        {
+            public TanhBackward(Tensor a, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = np.tanh(a.data);
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, 1 - np.power(data, 2));
+                Tensor.grads.Push(create);
+            }
+        }
+        class ReLUBackward : Tensor
+        {
+            public ReLUBackward(Tensor a, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = np.maximum(a.data, 0);
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, np.maximum(np.sign(a.data), 0));
+                Tensor.grads.Push(create);
+            }
+        }
+        class MeanBackward : Tensor
+        {
+            public MeanBackward(Tensor a, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = np.mean(a.data);
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, np.mean(a.data / data) / a.data.flatten().shape[0]);
+                Tensor.grads.Push(create);
+            }
+        }
+        class SumBackward : Tensor
+        {
+            public SumBackward(Tensor a, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = np.sum(a.data);
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, np.sum(a.data / data));
+                Tensor.grads.Push(create);
+            }
+        }
+        class ExpBackward : Tensor
+        {
+            public ExpBackward(Tensor a, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = np.exp(a.data);
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, data);
+                Tensor.grads.Push(create);
+            }
+        }
+        class LogBackward : Tensor
+        {
+            public LogBackward(Tensor a, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = np.log(a.data);
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, 1 / a.data);
+                Tensor.grads.Push(create);
+            }
+        }
+        class InvBackward : Tensor
+        {
+            public InvBackward(Tensor a, Stack<Dictionary<Tensor, NDArray>> grads)
+            {
+                data = 1 / a.data;
+                create = new Dictionary<Tensor, NDArray>();
+                Tensor.grads = grads;
+                create.Add(a, -np.power(a.data, -2));
+                Tensor.grads.Push(create);
+            }
+        }
+    }
+}
---
+++ TorchSharp/nn.cs
@@ -0,0 +1,197 @@
+﻿using NumSharp;
+using System;
+using System.Collections.Generic;
+namespace TorchSharp
+{
+    namespace nn
+    {
+        class Init
+        {
+            public static Tensor xavier_uniform(Tensor tensor, double gain)
+            {
+                double[] fans = calc_fan_in_and_fan_out(tensor);
+                double a = gain * Math.Sqrt(6 / (fans[0] + fans[1]));
+                NDArray array = np.random.uniform(-a, a, tensor.data.shape);
+                Tensor tensors = new Tensor(array);
+                return tensors;
+            }
+            public static Tensor xavier_normal(Tensor tensor, double gain)
+            {
+                double[] fans = calc_fan_in_and_fan_out(tensor);
+                double std = gain * Math.Sqrt(2 / (fans[0] + fans[1]));
+                NDArray array = np.random.normal(0, Math.Pow(std, 2.0), tensor.data.shape);
+                Tensor tensors = new Tensor(array);
+                return tensors;
+            }
+            public static Tensor kaiming_uniform(Tensor tensor, double a, string mode, string nonlinearity)
+            {
+                double[] fans = calc_fan_in_and_fan_out(tensor);
+                double gain = calculate_gain(nonlinearity, a);
+                double bound = 0;
+                if (mode == "fan_in")
+                    bound = gain * Math.Sqrt(3 / fans[0]);
+                else
+                    bound = gain * Math.Sqrt(3 / fans[1]);
+                NDArray array = np.random.uniform(-bound, bound, tensor.data.shape);
+                Tensor tensors = new Tensor(array);
+                return tensors;
+            }
+            public static Tensor kaiming_normal(Tensor tensor, double a, string mode, string nonlinearity)
+            {
+                double[] fans = calc_fan_in_and_fan_out(tensor);
+                double gain = calculate_gain(nonlinearity, a);
+                double std;
+                if (mode == "fan_in")
+                    std = gain / Math.Sqrt(fans[0]);
+                else
+                    std = gain / Math.Sqrt(fans[1]);
+                NDArray array = np.random.normal(0, Math.Pow(std, 2.0), tensor.data.shape);
+                Tensor tensors = new Tensor(array);
+                return tensors;
+            }
+            public static Tensor uniform(Tensor tensor, double a, double b)
+            {
+                NDArray array = np.random.uniform(a, b, tensor.data.shape);
+                Tensor tensors = new Tensor(array);
+                return tensors;
+            }
+            public static Tensor normal(Tensor tensor, double mean, double std)
+            {
+                NDArray array = np.random.normal(mean, std, tensor.data.shape);
+                Tensor tensors = new Tensor(array);
+                return tensors;
+            }
+            public static double calculate_gain(string nonlinearity, double parm)
+            {
+                double result = 0;
+                switch (nonlinearity)
+                {
+                    case "linear":
+                        result = 1;
+                        break;
+                    case "conv":
+                        result = 1;
+                        break;
+                    case "sigmoid":
+                        result = 1;
+                        break;
+                    case "tanh":
+                        result = 5 / 3;
+                        break;
+                    case "relu":
+                        result = Math.Sqrt(2.0);
+                        break;
+                    case "leaky_relu":
+                        result = Math.Sqrt(2 / (1 + Math.Pow(parm, 2.0)));
+                        break;
+                }
+                return result;
+            }
+            public static double[] calc_fan_in_and_fan_out(Tensor tensor)
+            {
+                double[] fans = new double[2];
+                int[] shape = tensor.data.shape;
+                if (shape.Length == 2)
+                {
+                    fans[0] = shape[1];
+                    fans[1] = shape[0];
+                }
+                else
+                {
+                    fans[0] = shape[1];
+                    fans[1] = shape[0];
+                    double size = 1;
+                    if (shape.Length > 2)
+                    {
+                        int[] numel = tensor.data[0, 0].shape;
+                        for (int i = 0; i < numel.Length; i++)
+                            size *= numel[i];
+                    }
+                    fans[0] *= size;
+                    fans[1] *= size;
+                }
+                return fans;
+            }
+        }
+        
+        public class Module 
+        {
+            public Tensor weight = new Tensor(np.zeros());
+            public Tensor bias = new Tensor(np.zeros());
+            public static List<Module> parameter = new List<Module>();
+            public virtual void register_parameter(Module module)
+            {
+                parameter.Add(module);
+            }
+            public virtual List<Module> parameters() 
+            {
+                return parameter;
+            }
+            public virtual Tensor forward(Tensor input) 
+            {
+                for (int i = 0; i < parameter.Count; i++) 
+                    input = parameter[i].forward(input);
+                return input;
+            }
+        }
+        public class Sequential : Module 
+        {
+            public Sequential(params Module[] module) 
+            {
+                for (int i = 0; i < module.Length; i++)
+                {
+                    register_parameter(module[i]);
+                }
+            }
+            public override List<Module> parameters()
+            {
+                return parameter;
+            }
+            public override Tensor forward(Tensor input)
+            {
+                for (int i = 0; i < parameter.Count; i++)
+                    input = parameter[i].forward(input);
+                return input;
+            }
+        }
+        public class Linear : Module
+        {
+            int in_feat;
+            int out_feat;
+            
+            public Linear(int in_feat, int out_feat) 
+            {
+                this.in_feat = in_feat;
+                this.out_feat = out_feat;
+                reset_parameter();
+            }
+            public void reset_parameter() 
+            {
+                Tensor weight_array = new Tensor(np.zeros(out_feat, in_feat));
+                Tensor bias_array = new Tensor(np.zeros(out_feat));
+                double[] fan =  Init.calc_fan_in_and_fan_out(weight_array);
+                double bound = 1 / Math.Sqrt(fan[0]);
+                weight = Init.kaiming_uniform(weight_array, Math.Sqrt(5), "fan_in", "relu");
+                bias = Init.uniform(bias_array, -bound, bound);
+            }
+            public override Tensor forward(Tensor input) 
+            {
+                return Tensor.linear(input, weight, bias);
+            }
+        }
+        public class ReLU : Module
+        {
+            public override Tensor forward(Tensor input)
+            {
+                return Tensor.relu(input);
+            }
+        }
+        public class MSELoss : Module
+        {
+            public Tensor forward(Tensor output, Tensor label)
+            {
+                return Tensor.mse_loss(output, label);
+            }
+        }
+    }
+}
